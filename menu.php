d608b3a6ff65a24edff7ed73086ba6310d79eaa1
Caroline-cphbusiness <caro1994@live.dk>
2016-09-23 11:19:16 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -6,7 +6,9 @@
 <link rel="stylesheet" type="text/css" href="css/styles.css">
 </header>
 <nav>
-    <a href="home.php" <?php if ($curpage == 'home.php'){echo 'class="active"';} ?>>Home</a>
+    <a href="home.php" <?php 
+                        // hvis den nuværende side er home.php, aktiveres class'en kaldt "active"
+                        if ($curpage == 'home.php'){echo 'class="active"';} ?>>Home</a>
     <a href="about.php" <?php if ($curpage == 'about.php'){echo 'class="active"';} ?>>About</a>
     <a href="projects.php" <?php if ($curpage == 'projects.php'){echo 'class="active"';} ?>>Projects</a>
 </nav>
